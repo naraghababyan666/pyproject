91371e0a5a170379978d91761d24a60dcddc897c
naraghababyan666 <naraghababyan666@gmail.com>
2022-08-17 17:23:43 +0400

    set_password not working

--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+
+from user.serializers import UserSerializer
+from rest_framework.views import APIView
+
+
+class Register(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+    def get(self, request):
+        return
